2acfe216136de861f0be3809a5e2206419cc9441
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-07-20 09:47:27 +0000

    WinformControlsDemo

---
+++ CSharpStudy/WinformControlsDemo/DBMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+
+class DBMaker
+{
+    public static DataTable GetCity()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add(new DataColumn("Id", typeof(int)));
+        dt.Columns.Add(new DataColumn("txtName", typeof(string)));
+        dt.Columns.Add(new DataColumn("ParentId", typeof(int)));
+        dt.Rows.Add(1, "北京", 0);
+        dt.Rows.Add(2, "山西", 0);
+
+        dt.Rows.Add(3, "朝阳区", 1);
+        dt.Rows.Add(4, "海淀区", 1);
+
+        dt.Rows.Add(5, "豆各庄", 3);
+        dt.Rows.Add(6, "十里堡", 3);
+        dt.Rows.Add(7, "中关村", 4);
+
+        dt.Rows.Add(8, "运城地区", 2);
+        dt.Rows.Add(9, "太原市", 2);
+        dt.Rows.Add(10, "永济市", 8);
+        dt.Rows.Add(11, "西文学", 10);
+        dt.Rows.Add(12, "东文学", 10);
+        dt.Rows.Add(13, "南文学", 10);
+        return dt;
+    }
+}
+
---
+++ CSharpStudy/WinformControlsDemo/FrmDataGridView.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WinformControlsDemo
+{
+    partial class FrmDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgv1.Location = new System.Drawing.Point(12, 42);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowTemplate.Height = 23;
+            this.dgv1.Size = new System.Drawing.Size(313, 152);
+            this.dgv1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "填充及添加ComboBox列";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "附值";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "取值";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(361, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "禁止列排序";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(361, 42);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowTemplate.Height = 23;
+            this.dgv2.Size = new System.Drawing.Size(330, 150);
+            this.dgv2.TabIndex = 5;
+            this.dgv2.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgv2_RowPostPaint);
+            // 
+            // FrmDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 210);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgv1);
+            this.Name = "FrmDataGridView";
+            this.Text = "FrmDataGridView";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgv2;
+    }
+}
