3f6028563adfea97f4d71c4fc7c939e37001f74f
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-07-21 03:37:30 +0000

    添加Configs目录，主要记录Web.config及App.config常用配置结点及相关读写删操作

---
+++ trunk/Configs/Opt.cs
@@ -0,0 +1,20 @@
+﻿对于WINFORM程序，使用 System.Configuration.ConfigurationManager；
+对于ASP.NET 程序， 使用 System.Web.Configuration.WebConfigurationManager；
+
+ASP.NET(需要有写权限):
+
+Configuration config = WebConfigurationManager.OpenWebConfiguration(null);
+AppSettingsSection app = config.AppSettings;
+app.Settings.Add("x", "this is X");//添加
+app.Settings["x"].Value = "this is not Y";//修改
+app.Settings.Remove("x");//删除
+config.Save(ConfigurationSaveMode.Modified);
+
+WinForm:
+
+Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+AppSettingsSection app = config.AppSettings;
+app.Settings.Add("x", "this is X");
+app.Settings["x"].Value = "this is not Y";
+app.Settings.Remove("x");
+config.Save(ConfigurationSaveMode.Modified);
