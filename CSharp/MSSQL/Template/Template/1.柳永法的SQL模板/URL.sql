a6e3393874421d39bb444894fe5d5557fa8527ac
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-11-14 09:25:07 +0000

    增加SQL相关链接

--- CSharp/MSSQL/Template/Template/1.柳永法的SQL模板/URL.sql
+++ CSharp/MSSQL/Template/Template/1.柳永法的SQL模板/URL.sql
@@ -1,7 +1,7 @@
 ﻿http://topic.csdn.net/u/20090117/09/76c4d3d9-552c-4803-b77a-5708d2e8e8cd.html
 http://topic.csdn.net/u/20080920/15/61bf31bf-518c-41be-9e4a-b166c878dcaf.html
 http://forum.csdn.net/BList/MSSQL/EssentialList
-
+http://topic.csdn.net/u/20090310/17/0b8f60c5-9304-4633-84fa-d176f282cb7d.html
 
 
 查询性能
