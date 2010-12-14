79c1cde7edbdc339c97ac7f63793e87bc0c28bd5
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-12-14 15:44:19 +0000

    主要增加Dynamic

--- trunk/CSharp/CSharpStudy/算法/二进制，8进制，10进制，16进制.cs
+++ trunk/CSharp/CSharpStudy/算法/二进制，8进制，10进制，16进制.cs
@@ -9,33 +9,33 @@ namespace 算法
     class 二进制_8进制_10进制_16进制
     {
 
-        static void Main(string[] args)
-        {
-            //int count = 10000;
-            //int value = int.MaxValue / 123;
+        //static void Main(string[] args)
+        //{
+        //int count = 10000;
+        //int value = int.MaxValue / 123;
 
-            //run(value, 2, count, IntToString);
-            //run(value, 2, count, IntToString2);
+        //run(value, 2, count, IntToString);
+        //run(value, 2, count, IntToString2);
 
-            //run(value, 2, count, IntToStringByNet);
-
-
-            ////十进制转二进制
-            //Console.WriteLine(Convert.ToString(value, 2));
-            ////十进制转八进制
-            //Console.WriteLine(Convert.ToString(value, 8));
-            ////十进制转十六进制
-            //Console.WriteLine(Convert.ToString(value, 16));
-
-            ////二进制转十进制
-            //Console.WriteLine(Convert.ToInt32("100111101", 2));
-            ////八进制转十进制
-            //Console.WriteLine(Convert.ToInt32("76", 8));
-            ////十六进制转十进制
-            //Console.WriteLine(Convert.ToInt32("FF", 16));
+        //run(value, 2, count, IntToStringByNet);
 
 
-        }
+        ////十进制转二进制
+        //Console.WriteLine(Convert.ToString(value, 2));
+        ////十进制转八进制
+        //Console.WriteLine(Convert.ToString(value, 8));
+        ////十进制转十六进制
+        //Console.WriteLine(Convert.ToString(value, 16));
+
+        ////二进制转十进制
+        //Console.WriteLine(Convert.ToInt32("100111101", 2));
+        ////八进制转十进制
+        //Console.WriteLine(Convert.ToInt32("76", 8));
+        ////十六进制转十进制
+        //Console.WriteLine(Convert.ToInt32("FF", 16));
+
+
+        //}
         public delegate string DelegateMethod(int value, int toBase);
 
         public static void run(int value, int toBase, int count, DelegateMethod method)
