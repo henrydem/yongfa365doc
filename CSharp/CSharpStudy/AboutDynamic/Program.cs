c73abeb969d286430e9f309096262f97cd5002cf
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-12-14 15:44:19 +0000

    主要增加Dynamic

---
+++ CSharp/CSharpStudy/AboutDynamic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace AboutDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic user = new ExpandoObject();
+            user.UserName = "柳永法";
+            user.Sex = "男";
+            user.Birthday = DateTime.Now;
+            user.Money = 123.456m;
+
+            Console.WriteLine(user.UserName);
+
+            dynamic user2 = new { UserName = "柳永法", Age = 28, Birthday = DateTime.Now };
+            Console.WriteLine(user2.UserName);
+
+            var user3 = new { UserName = "柳永法", Age = 28, Birthday = DateTime.Now };
+            Console.WriteLine(user3.UserName);
+        }
+    }
+}
