58ede6f4e44ee54ae91c42a5d6a3860b3b3e3ec3
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2010-12-19 05:23:23 +0000

    Serializable深入学习相关特性。以前觉得可以序列化，反序列化就OK了，要什么特性，正式使用时，发现很多人都用特性，所以总找不到错的原因，想不明白为什么反序列化不成功，原来没有设置特性。特性改变了原有的对象生成XML的方式，把xml都改名了。
    还是MSDN上的全。

---
+++ trunk/CSharp/CSharpStudy/AboutSerializable/SerializeXmlNoAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using YongFa365.Serialization;
+using System.Xml.Schema;
+
+namespace AboutSerializable
+{
+    public class SerializeXmlNoAttribute
+    {
+        public void Test()
+        {
+            Student2 student = new Student2()
+            {
+                Code = 1,
+                Age = 23,
+                ComeIn = DateTime.Now,
+                UserName = "张三",
+                Scores = new List<StudentScore2>()
+                {
+                    new StudentScore2{ Code=1, Score=34.3m, Course="语言"},
+                    new StudentScore2{ Code=1, Score=50.4m, Course="化学"},
+                    new StudentScore2{ Code=1,Course=""},
+                }
+            };
+
+            SerializeHelper serial = new SerializeHelper();
+            string temp = serial.ToXml3(student);
+            Student student2 = serial.FromXml<Student>(temp);
+
+        }
+    }
+
+
+    [Serializable]
+    public class Student2
+    {
+        public int Code { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public DateTime ComeIn { get; set; }
+        public List<StudentScore2> Scores { get; set; }
+    }
+
+    [Serializable]
+    public class StudentScore2
+    {
+        public int Code { get; set; }
+        public string Course { get; set; }
+        public decimal Score { get; set; }
+    }
+}
