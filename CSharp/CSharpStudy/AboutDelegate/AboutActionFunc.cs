4b1091bb871c1c5a58ccadef12fe2348298c7d6a
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-04-09 14:30:00 +0000

    AboutActionFunc.cs 总是使用，但都是习惯，这次深入研究了下，Action<T....>,Func<T...>真的很好用。

---
+++ CSharp/CSharpStudy/AboutDelegate/AboutActionFunc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AboutDelegate
+{
+    class AboutActionFunc
+    {
+        public static void Run()
+        {
+            Action<int, int> ActionCalc = (a, b) => { Console.WriteLine(a + b); };
+            ActionCalc(1, 2);
+
+
+
+            Func<int, int, int> FuncCalc = (a, b) => { return a + b; };
+            Console.WriteLine(FuncCalc(1, 2));
+
+
+            Func<Users, int, string, Users> FuncUsers = (u, age, username) =>
+            {
+                u.Age = age;
+                u.UserName = username;
+                return u;
+            };
+
+            var users = FuncUsers(new Users(), 28, "柳");
+
+        }
+
+    }
+
+    class Users
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+    }
+
+}
