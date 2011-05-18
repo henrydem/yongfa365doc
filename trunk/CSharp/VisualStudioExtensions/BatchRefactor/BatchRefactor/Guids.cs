c7e3431cd0e032e88479124a366d27b89eb71c3f
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-05-18 15:44:25 +0000

    VisualStudioExtensions VS2010扩展开发 .BatchRefactor

---
+++ trunk/CSharp/VisualStudioExtensions/BatchRefactor/BatchRefactor/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace YongFa365.BatchRefactor
+{
+    static class GuidList
+    {
+        public const string guidBatchRefactorPkgString = "ca45914b-4959-408a-98cf-87e4f9e97815";
+        public const string guidBatchRefactorCmdSetString = "70fd9bbd-df20-4bc5-b51c-eaa35b408b7c";
+
+        public static readonly Guid guidBatchRefactorCmdSet = new Guid(guidBatchRefactorCmdSetString);
+    };
+}
---
+++ trunk/CSharp/VisualStudioExtensions/BatchRefactor/BatchRefactor/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace YongFa365.BatchRefactor
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+
+
+    };
+}
