a0de3f78f6d2665e674066b7357307d38777d730
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-05-19 13:55:42 +0000

    Temp

--- trunk/CSharp/VisualStudioExtensions/BatchRefactor/BatchRefactor/PkgCmdID.cs
+++ trunk/CSharp/VisualStudioExtensions/BatchRefactor/BatchRefactor/PkgCmdID.cs
@@ -6,8 +6,11 @@ namespace YongFa365.BatchRefactor
 {
     static class PkgCmdIDList
     {
-        public const uint cmdidMyCommand =        0x100;
-
-
+        public const uint cmdidRemoveUnusedUsings = 0x2001;
+        public const uint cmdidSortUsings = 0x2002;
+        public const uint cmdidRemoveAndSortUsings = 0x2003;
+        public const uint cmdidFormatDocument = 0x2004;
+        public const uint cmdidSortUsingsAndFormatDocument = 0x2005;
+        public const uint cmdidRemoveAndSortUsingsAndFormatDocument = 0x2006;
     };
 }
