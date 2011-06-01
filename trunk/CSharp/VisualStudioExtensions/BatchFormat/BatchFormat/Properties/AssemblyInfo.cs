e8be74c19e0691bf43e669e7849247673829128b
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-06-01 14:28:07 +0000

    国际化，全部使用英文

--- trunk/CSharp/VisualStudioExtensions/BatchFormat/BatchFormat/Properties/AssemblyInfo.cs
+++ trunk/CSharp/VisualStudioExtensions/BatchFormat/BatchFormat/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("YongFa365")]
 [assembly: AssemblyProduct("BatchFormat")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("http://www.yongfa365.com/")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 
 
