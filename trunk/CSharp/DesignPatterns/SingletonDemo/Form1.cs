82e94fdc221d6772b637c3d0902bc2b391f5bbd2
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-06-19 08:57:46 +0000

    Add DesignPatterns.SingletonDemo

---
+++ trunk/CSharp/DesignPatterns/SingletonDemo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingletonDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //FrmToolBox frm = new FrmToolBox();  //不能这么调用
+
+            FrmToolBox frm = FrmToolBox.Instance;
+            frm.MdiParent = this;
+            frm.Show();
+
+        }
+
+        private void 打开工具栏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmToolBox frm = FrmToolBox.Instance;
+            frm.MdiParent = this;
+            frm.Show();
+
+        }
+    }
+}
---
+++ trunk/CSharp/DesignPatterns/SingletonDemo/frmToolBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingletonDemo
+{
+    public partial class FrmToolBox : Form
+    {
+        private static readonly FrmToolBox instance = new FrmToolBox();
+
+        public static FrmToolBox Instance { get { return instance; } }
+
+        private FrmToolBox()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void FrmToolBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+
+        }
+    }
+}
