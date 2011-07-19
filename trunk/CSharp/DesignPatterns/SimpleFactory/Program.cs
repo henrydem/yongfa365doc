e08d69c649a9e4694859600175a854ad26a8c8b2
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-07-19 13:39:47 +0000

    简单工厂模式

---
+++ trunk/CSharp/DesignPatterns/SimpleFactory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("请输入数字A：");
+                string strNumberA = Console.ReadLine();
+                Console.Write("请选择运算符号(+、-、*、/)：");
+                string strOperate = Console.ReadLine();
+                Console.Write("请输入数字B：");
+                string strNumberB = Console.ReadLine();
+                string strResult = "";
+
+                Operation oper = OperationFactory.CreateOperate(strOperate);
+                oper.NumberA = Convert.ToDouble(strNumberA);
+                oper.NumberB = Convert.ToDouble(strNumberB);
+                strResult = oper.GetResult().ToString();
+
+                Console.WriteLine("结果是：" + strResult);
+
+                Console.ReadLine();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("您的输入有错：" + ex.Message);
+            }
+
+        }
+    }
+}
