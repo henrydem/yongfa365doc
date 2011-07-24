099e0c46a9a3deff651a4b729102515406c5715f
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-07-24 04:22:35 +0000

    休息提醒，主体

---
+++ CSharp/休息提醒/休息提醒/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace 锁定计算机
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRun = new System.Windows.Forms.Button();
+            this.cbxTime = new System.Windows.Forms.ComboBox();
+            this.cbxOpt = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(1, 32);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(100, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "执行";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // cbxTime
+            // 
+            this.cbxTime.FormattingEnabled = true;
+            this.cbxTime.Items.AddRange(new object[] {
+            "每30分钟",
+            "每60分钟",
+            "每90分钟",
+            "每120分钟",
+            "每150分钟",
+            "每180分钟"});
+            this.cbxTime.Location = new System.Drawing.Point(1, 6);
+            this.cbxTime.Name = "cbxTime";
+            this.cbxTime.Size = new System.Drawing.Size(100, 20);
+            this.cbxTime.TabIndex = 1;
+            // 
+            // cbxOpt
+            // 
+            this.cbxOpt.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOpt.FormattingEnabled = true;
+            this.cbxOpt.Items.AddRange(new object[] {
+            "关闭显示器",
+            "锁定计算机",
+            "锁定+黑屏"});
+            this.cbxOpt.Location = new System.Drawing.Point(117, 6);
+            this.cbxOpt.Name = "cbxOpt";
+            this.cbxOpt.Size = new System.Drawing.Size(104, 20);
+            this.cbxOpt.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(117, 32);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(104, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 60);
+            this.Controls.Add(this.cbxOpt);
+            this.Controls.Add(this.cbxTime);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "休息提醒";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ComboBox cbxTime;
+        private System.Windows.Forms.ComboBox cbxOpt;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ CSharp/休息提醒/休息提醒/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace 锁定计算机
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern void LockWorkStation();
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(int hwnd, int wMsg, int wParam, int lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxTime.SelectedIndex = 0;
+            cbxOpt.SelectedIndex = 0;
+            foreach (var item in Environment.GetCommandLineArgs())
+            {
+                if (item.ToLower()=="/start")
+                {
+                    Start();
+                }
+                else if (item.ToLower()=="/stop")
+                {
+                    Stop();
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        void Start()
+        {
+            timer1.Interval = Convert.ToInt32(Regex.Match(cbxTime.Text, "\\d+").Value) * 60 * 1000;
+            timer1.Enabled = true;
+            btnRun.Enabled = false;
+            btnStop.Enabled = true;
+            cbxOpt.Enabled = false;
+            cbxTime.Enabled = false;
+        
+        }
+
+        void Stop()
+        {
+            timer1.Enabled = false;
+            btnRun.Enabled = true;
+            btnStop.Enabled = false;
+
+            cbxOpt.Enabled = true;
+            cbxTime.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cbxOpt.Text == "关闭显示器")
+            {
+                SendMessage(this.Handle.ToInt32(), 0x112, 0xF170, 2); //关闭   
+               // SendMessage(this.Handle.ToInt32(), 0x112, 0xF170, -1); //打开
+            }
+            else if (cbxOpt.Text == "锁定计算机")
+            {
+                LockWorkStation();
+            }
+            else if (cbxOpt.Text == "锁定+黑屏")
+            {
+                LockWorkStation();
+                SendMessage(this.Handle.ToInt32(), 0x112, 0xF170, 2); 
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.Activate();
+        }
+    }
+}
