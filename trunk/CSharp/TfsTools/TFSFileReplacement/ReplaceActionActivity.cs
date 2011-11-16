098ae876ce155a20aa1d959ed394170c57d60b7f
yongfa365 <yongfa365@399c6538-a716-181a-cdfa-c1aa239b2f65>
2011-11-16 13:41:17 +0000

    添加： TfsTools,有两个功能，发布或生成软件时替换文件版本号，另一个根据正则表达式替换。

---
+++ trunk/CSharp/TfsTools/TFSFileReplacement/ReplaceActionActivity.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Microsoft.TeamFoundation.Build.Client;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TFSFileReplacement
+{
+    [BuildActivity(HostEnvironmentOption.Agent)]
+    public sealed class ReplaceActionActivity : CodeActivity
+    {
+        /// <summary>
+        /// 替换规则集，模板：处理扩展名-_-匹配-_-替换@_@处理扩展名1-_-匹配1-_-替换1
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<string> Rules { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<string> FilePath { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var orgRule = Rules.Get(context);
+            orgRule = Regex.Replace(orgRule, @"\$\(Date:(.+?)\)", new MatchEvaluator((m) =>
+            {
+                if (m.Success && m.Groups.Count == 2)
+                {
+                    return DateTime.Now.ToString(m.Groups[1].Value);
+                }
+                return string.Empty;
+            }));
+            var rules = orgRule.Split(new string[] { "@_@", "-_-" }, StringSplitOptions.None);
+            var filePath = FilePath.Get(context);
+
+
+            if (String.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("路径不能为空", "FilePath");
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("文件不存在", filePath);
+            }
+
+
+            for (int i = 0; i < rules.Length; i += 3)
+            {
+                var rule = new Param { Extensions = rules[i].Split(';'), RegularExpression = rules[i + 1], Replacement = rules[i + 2] };
+                if (rule.Extensions.Any((ex) => { return Regex.IsMatch(filePath, ex, RegexOptions.IgnoreCase); }))
+                {
+                    var re = new Regex(rule.RegularExpression, RegexOptions.IgnoreCase);
+                    var body = File.ReadAllText(filePath);
+                    if (re.IsMatch(body))
+                    {
+                        FileAttributes fileAttributes = File.GetAttributes(filePath);
+                        File.SetAttributes(filePath, fileAttributes & ~FileAttributes.ReadOnly);
+                        String contents = re.Replace(body, rule.Replacement);
+                        File.WriteAllText(filePath, contents);
+                        File.SetAttributes(filePath, fileAttributes);
+                    }
+                }
+            }
+        }
+
+
+
+
+        private class Param
+        {
+            /// <summary>
+            /// 匹配的扩展名,如".aspx;.ascx"
+            /// </summary>
+            public string[] Extensions { get; set; }
+
+            /// <summary>
+            /// 匹配
+            /// </summary>
+            public string RegularExpression { get; set; }
+
+            /// <summary>
+            /// 替换
+            /// </summary>
+            public string Replacement { get; set; }
+
+        }
+    }
+}
---
+++ trunk/CSharp/TfsTools/TfsReplaceAssemblyInfo/ReplaceInFile.cs
@@ -0,0 +1,112 @@
+﻿namespace TfsReplaceAssemblyInfo
+{
+    using System;
+    using System.Activities;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    using Microsoft.TeamFoundation.Build.Client;
+
+    /// <summary>
+    /// Workflow activity that replaces all occurrences of a regular expression
+    /// in a text file with the specified text.
+    /// </summary>
+    [BuildActivity(HostEnvironmentOption.Agent)]
+    public sealed class ReplaceInFile : CodeActivity
+    {
+        #region Properties
+
+        /// <summary>
+        /// Specify the path of the file to replace occurences of the regular 
+        /// expression with the replacement text
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<string> FilePath
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Regular expression to search for and replace in the specified
+        /// text file.
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<string> RegularExpression
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Text to replace occurrences of the specified regular expression with.
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<string> Replacement
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Executes the logic for this workflow activity
+        /// </summary>
+        /// <param name="context">Workflow context</param>
+        /// <exception cref="ArgumentException">If the specified context does not
+        /// contain a FilePath and a RegularExpression, an ArgumentException 
+        /// is thrown.</exception>
+        protected override void Execute(CodeActivityContext context)
+        {
+            // get the value of the FilePath in argument from the workflow context
+            String filePath = FilePath.Get(context);
+
+            // throw if the file path is null or empty
+            if (String.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException(
+                    "Specify a path to replace text in", "FilePath");
+            }
+
+            // throw if the specified file path doesn't exist
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(
+                    "File not found", filePath);
+            }
+
+            // get the value of the RegularExpression in argument
+            String regularExpression = context.GetValue(RegularExpression);
+
+            // throw if the regular expression is null or empty
+            if (String.IsNullOrEmpty(regularExpression))
+            {
+                throw new ArgumentException(
+                    "Specify an expression to replace", "RegularExpression");
+            }
+
+
+            // ensure that the file is writeable
+            FileAttributes fileAttributes = File.GetAttributes(filePath);
+            File.SetAttributes(filePath, fileAttributes & ~FileAttributes.ReadOnly);
+
+            // perform the actual replacement
+            String contents = Regex.Replace(
+                File.ReadAllText(filePath),
+                @"AssemblyFileVersion\(""" + regularExpression,
+                @"AssemblyFileVersion(""" + Replacement.Get(context) ?? String.Empty
+                );
+           // String contents = regex.Replace(File.ReadAllText(filePath), replacement);
+
+            File.WriteAllText(filePath, contents);
+
+            // restore the file's original attributes
+            File.SetAttributes(filePath, fileAttributes);
+        }
+
+        #endregion Methods
+    }
+}
