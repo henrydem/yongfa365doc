6e81b2955e2d11b3a0f61f39afeda8df278ef882
yongfa365@gmail.com <yongfa365@gmail.com@399c6538-a716-181a-cdfa-c1aa239b2f65>
2012-10-08 05:53:53 +0000

    add Silverlight Extensions

---
+++ trunk/CSharp/SilverlightDemos/SilverlightExtensions/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightExtensions
+{
+    public class Extension
+    {
+
+        public static void BindSource(this DataPager dataPager, int totalCount, int pageSize)
+        {
+            var list = new List<int>(totalCount);
+            for (int i = 0; i < totalCount; i++) list.Add(i);
+            dataPager.Source = new PagedCollectionView(list)
+            {
+                PageSize = pageSize
+            };
+        }
+
+
+    }
+}
