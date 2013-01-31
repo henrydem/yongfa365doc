11185d2a5f421856c35927dfb6001f9edd9dbfd8
yongfa365@gmail.com <yongfa365@gmail.com@399c6538-a716-181a-cdfa-c1aa239b2f65>
2013-01-31 15:23:59 +0000

    全组合算法

---
+++ trunk/CSharp/CSharpStudy/算法/全组合算法.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 算法
+{
+    class 全组合算法
+    {
+        [Flags]
+        public enum PersonType
+        {
+            Audit = 1,
+            Child = 2,
+            Senior = 4
+        }
+
+        public static void Run(string[] args)
+        {
+            var lstSource = GetEnumList<PersonType>();
+            var lstComb = FullCombination(lstSource);
+            var lstResult = new List<PersonType>();
+            lstComb.ForEach(item => lstResult.Add(item.Aggregate((result, source) => result | source)));
+        }
+
+        private static List<T> GetEnumList<T>()
+        {
+            var lst = new List<T>();
+            foreach (T item in Enum.GetValues(typeof(T)))
+            {
+                lst.Add(item);
+            }
+            return lst;
+        }
+
+        private static List<List<T>> FullCombination<T>(List<T> lstSource)
+        {
+            var n = lstSource.Count;
+            var max = 1 << n;
+            var lstResult = new List<List<T>>();
+            for (var i = 0; i < max; i++)
+            {
+                var lstTemp = new List<T>();
+                for (var j = 0; j < n; j++)
+                {
+                    if ((i >> j & 1) > 0)
+                    {
+                        lstTemp.Add(lstSource[j]);
+                    }
+                }
+                lstResult.Add(lstTemp);
+            }
+            lstResult.RemoveAt(0);
+            return lstResult;
+        }
+
+    }
+}
